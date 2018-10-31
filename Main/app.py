7cc645233b18baccd1b0c1f2d62f2756ff144d27
Ben Kedl <benj.kedl@gmail.com>
2018-10-30 21:50:43 -0700

    saving edits

--- Main/app.py
+++ Main/app.py
@@ -4,38 +4,45 @@ from flask import (
     jsonify,
     request,
     redirect)
+import pymongo
+import pandas as pd
 
 # flask setup
 app = Flask(__name__)
 
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+db = client.crime_db
+collection = db.violent_crime
 # template rendering
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/choropleth")
-def home():
+@app.route("/choropleth1")
+def choro1():
     return render_template("VisJH1.html")
 
-@app.route("/choropleth")
-def home():
+@app.route("/choropleth2")
+def choro2():
     return render_template("VisJH2.html")
 
-@app.route("/scatter")
-def home():
+@app.route("/scatter1")
+def scatter1():
     return render_template("VisSS1.html")
 
-@app.route("/scatter")
-def home():
+@app.route("/scatter2")
+def scatter2():
     return render_template("VisSS2.html")
 
-@app.route("/chartjs")
-def home():
-    return render_template("VisBK1.html")
+@app.route("/chartjs1")
+def chart1():
+    return render_template("timelines.html")
 
-@app.route("/chartjs")
-def home():
-    return render_template("VisBK2.html")
+# @app.route("/chartjs2")
+# def chart2():
+#     return render_template("VisBK2.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
