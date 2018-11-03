91b4a4b22abe42c21965fb511fb5fedafeb72c12
Akashalives7 <35281785+Akashalives7@users.noreply.github.com>
2018-11-02 21:00:47 -0700

    New links to app for visualizations

--- Main/app.py
+++ Main/app.py
@@ -10,44 +10,44 @@ import pandas as pd
 # flask setup
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
+# conn = "mongodb://localhost:27017"
+# client = pymongo.MongoClient(conn)
 
-db = client.crime_db
-collection = db.violent_crime
+# db = client.crime_db
+# collection = db.violent_crime
 
-#bens weapons data gathering:
-df = pd.DataFrame(list(db.collection.find({})))
-all_weapons = df['weapon_desc'].unique()
-all_weapons
+# #bens weapons data gathering:
+# df = pd.DataFrame(list(db.collection.find({})))
+# all_weapons = df['weapon_desc'].unique()
+# all_weapons
 
-auto_guns = [ 'UZI SEMIAUTOMATIC ASSAULT RIFLE','UNK TYPE SEMIAUTOMATIC ASSAULT RIFLE',
-       'HECKLER & KOCH 91 SEMIAUTOMATIC ASSAULT RIFLE','MAC-10 SEMIAUTOMATIC ASSAULT WEAPON','HECKLER & KOCH 93 SEMIAUTOMATIC ASSAULT RIFLE','MAC-11 SEMIAUTOMATIC ASSAULT WEAPON','ASSAULT WEAPON/UZI/AK47/ETC','OTHER FIREARM','AUTOMATIC WEAPON/SUB-MACHINE GUN','SEMI-AUTOMATIC RIFLE','SEMI-AUTOMATIC PISTOL',]
-nonauto_guns = ['SAWED OFF RIFLE/SHOTGUN','RIFLE','UNKNOWN FIREARM', 'HAND GUN', 'SHOTGUN', 'REVOLVER']
-handheld = [ 
-           'RELIC FIREARM',
-       'ANTIQUE FIREARM','MARTIAL ARTS WEAPONS', 'BLACKJACK', 'BOW AND ARROW','STRAIGHT RAZOR', 'BOWIE KNIFE', 'CLEAVER', 'RAZOR BLADE',
-       'ICE PICK', 'BRASS KNUCKLES',
-       'STUN GUN','SWITCH BLADE', 'RAZOR','GLASS',
-       'AXE','BELT FLAILING INSTRUMENT/CHAIN','SCISSORS','TIRE IRON', 'TOY GUN',
-       'AIR PISTOL/REVOLVER/RIFLE/BB GUN','STARTER PISTOL/REVOLVER','KNIFE WITH BLADE OVER 6 INCHES IN LENGTH',
-        'MACHETE',
-        'BOTTLE', 'SYRINGE', 'DIRK/DAGGER',
-       'BOARD', 'SCREWDRIVER','CLUB/BAT', 'UNKNOWN TYPE CUTTING INSTRUMENT', 'SWORD','BLUNT INSTRUMENT','STICK', 'HAMMER', 'KNIFE WITH BLADE 6INCHES OR LESS','OTHER CUTTING INSTRUMENT','ROCK/THROWN OBJECT','PIPE/METAL PIPE','KITCHEN KNIFE','FOLDING KNIFE', 'UNKNOWN WEAPON/OTHER WEAPON','OTHER KNIFE','CONCRETE BLOCK/BRICK']
-explosive = [ 'BOMB THREAT','EXPLOXIVE DEVICE',]
+# auto_guns = [ 'UZI SEMIAUTOMATIC ASSAULT RIFLE','UNK TYPE SEMIAUTOMATIC ASSAULT RIFLE',
+#        'HECKLER & KOCH 91 SEMIAUTOMATIC ASSAULT RIFLE','MAC-10 SEMIAUTOMATIC ASSAULT WEAPON','HECKLER & KOCH 93 SEMIAUTOMATIC ASSAULT RIFLE','MAC-11 SEMIAUTOMATIC ASSAULT WEAPON','ASSAULT WEAPON/UZI/AK47/ETC','OTHER FIREARM','AUTOMATIC WEAPON/SUB-MACHINE GUN','SEMI-AUTOMATIC RIFLE','SEMI-AUTOMATIC PISTOL',]
+# nonauto_guns = ['SAWED OFF RIFLE/SHOTGUN','RIFLE','UNKNOWN FIREARM', 'HAND GUN', 'SHOTGUN', 'REVOLVER']
+# handheld = [ 
+#            'RELIC FIREARM',
+#        'ANTIQUE FIREARM','MARTIAL ARTS WEAPONS', 'BLACKJACK', 'BOW AND ARROW','STRAIGHT RAZOR', 'BOWIE KNIFE', 'CLEAVER', 'RAZOR BLADE',
+#        'ICE PICK', 'BRASS KNUCKLES',
+#        'STUN GUN','SWITCH BLADE', 'RAZOR','GLASS',
+#        'AXE','BELT FLAILING INSTRUMENT/CHAIN','SCISSORS','TIRE IRON', 'TOY GUN',
+#        'AIR PISTOL/REVOLVER/RIFLE/BB GUN','STARTER PISTOL/REVOLVER','KNIFE WITH BLADE OVER 6 INCHES IN LENGTH',
+#         'MACHETE',
+#         'BOTTLE', 'SYRINGE', 'DIRK/DAGGER',
+#        'BOARD', 'SCREWDRIVER','CLUB/BAT', 'UNKNOWN TYPE CUTTING INSTRUMENT', 'SWORD','BLUNT INSTRUMENT','STICK', 'HAMMER', 'KNIFE WITH BLADE 6INCHES OR LESS','OTHER CUTTING INSTRUMENT','ROCK/THROWN OBJECT','PIPE/METAL PIPE','KITCHEN KNIFE','FOLDING KNIFE', 'UNKNOWN WEAPON/OTHER WEAPON','OTHER KNIFE','CONCRETE BLOCK/BRICK']
+# explosive = [ 'BOMB THREAT','EXPLOXIVE DEVICE',]
 
-physical = ['PHYSICAL PRESENCE','STRONG-ARM (HANDS, FIST, FEET OR BODILY FORCE)','VERBAL THREAT','MACE/PEPPER SPRAY',]
-other = ['VEHICLE','DEMAND NOTE','SIMULATED GUN','FIRE','ROPE/LIGATURE','LIQUOR/DRUGS','FIXED OBJECT',
-       'CAUSTIC CHEMICAL/POISON','SCALDING LIQUID','DOG/ANIMAL (SIC ANIMAL ON)']
+# physical = ['PHYSICAL PRESENCE','STRONG-ARM (HANDS, FIST, FEET OR BODILY FORCE)','VERBAL THREAT','MACE/PEPPER SPRAY',]
+# other = ['VEHICLE','DEMAND NOTE','SIMULATED GUN','FIRE','ROPE/LIGATURE','LIQUOR/DRUGS','FIXED OBJECT',
+#        'CAUSTIC CHEMICAL/POISON','SCALDING LIQUID','DOG/ANIMAL (SIC ANIMAL ON)']
 
-weapon_categories = [auto_guns, nonauto_guns, handheld, physical, other, explosive]
-weapon_names = ['auto_guns',
-        'nonauto_guns',
-        'handheld',
-        'physical',
-        'other',
-        'explosive'
-        ]
+# weapon_categories = [auto_guns, nonauto_guns, handheld, physical, other, explosive]
+# weapon_names = ['auto_guns',
+#         'nonauto_guns',
+#         'handheld',
+#         'physical',
+#         'other',
+#         'explosive'
+#         ]
 
 
 # template rendering
@@ -61,17 +61,13 @@ def choro1():
 
 @app.route("/choropleth2")
 def choro2():
-    return render_template("VisJH2.html")
+    return render_template("crime_rate_change.html")
 
 @app.route("/scatter1")
 def scatter1():
-    return render_template("VisSS1.html")
+    return render_template("index-DS.html")
 
-@app.route("/scatter2")
-def scatter2():
-    return render_template("VisSS2.html")
-
-@app.route("/<zipcode>")
+@app.route("/zipcode")
 def chart1(zipcode):
     # df = pd.DataFrame(list(db.collection.find({})))
     # zip_df = df[df['Zip Code']==90013]
